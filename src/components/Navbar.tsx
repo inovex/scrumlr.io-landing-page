3581790b595392877d59a98ef40b8f02eb279f7b
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-06-29 16:09:38 +0200

    Add scss for navbar and hydrate component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
+import classNames from 'classnames';
+import './Navbar.scss';
 
 export default function Navbar() {
+  const [isNavbarTransparent, setIsNavbarTransparent] = useState(true);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setIsNavbarTransparent(false);
+      } else {
+        setIsNavbarTransparent(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <nav>
+    <nav className={classNames('navbar', {'navbar--opaque': !isNavbarTransparent})}>
       <ul>
         <li>Scrumlr</li>
         <li>Startseite</li>
