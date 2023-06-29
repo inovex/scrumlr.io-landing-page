0243b29b3c66dc7a46ac51c23b2b34cb71614146
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-06-29 15:13:32 +0200

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Navbar() {
+
+  return (
+    <nav>
+      <ul>
+        <li>Scrumlr</li>
+        <li>Startseite</li>
+        <li>Funktionen</li>
+        <li>Über uns</li>
+        <li>Board erstellen +</li>
+      </ul>
+    </nav>
+  );
+}
