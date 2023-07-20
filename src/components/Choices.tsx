b473390f299908e5c8295a79e9d3be60de04332f
Resaki1 <christian.reski@t-online.de>
2023-07-20 12:02:40 +0200

    added graphics

--- src/components/Choices.tsx
+++ src/components/Choices.tsx
@@ -1,13 +1,37 @@
-import "./Choices.scss"
+import { useEffect, useState } from "react";
+import "./Choices.scss";
 
 const Choices = () => {
-    return (
-        <div className="choices">
-            <h2>Your board - your choices.</h2>
-            <p>Choose any type of retrospective that suits your team.</p>
-            <div className="choices__content">test</div>
+  const [position, setPosition] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPosition((prevPosition) => {
+        if (prevPosition === 8) {
+          return 0;
+        }
+
+        return prevPosition + 1;
+      });
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [position]);
+
+  return (
+    <div className="choices" id="Features">
+      <h2>Your board - your choices.</h2>
+      <p>Choose any type of retrospective that suits your team. {position}</p>
+      <div className="choices__content">
+        <div className="choices__buttons">
+          <button onClick={() => setPosition(0)}>Collaborate</button>
+          <button onClick={() => setPosition(3)}>Moderate</button>
+          <button onClick={() => setPosition(6)}>Customize</button>
         </div>
-    )
-}
+        <img src={`assets/choices/choices_${position}.svg`} alt="choices_1" />
+      </div>
+    </div>
+  );
+};
 
 export default Choices;
