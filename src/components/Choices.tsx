04713bad4c5e3515928c6091f31cf1acf677d570
Resaki1 <christian.reski@t-online.de>
2023-07-20 10:28:57 +0200

    added basic choices component

---
+++ src/components/Choices.tsx
@@ -0,0 +1,13 @@
+import "./Choices.scss"
+
+const Choices = () => {
+    return (
+        <div className="choices">
+            <h2>Your board - your choices.</h2>
+            <p>Choose any type of retrospective that suits your team.</p>
+            <div className="choices__content">test</div>
+        </div>
+    )
+}
+
+export default Choices;
