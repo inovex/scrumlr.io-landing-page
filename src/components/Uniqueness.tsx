f589c780f179243d31da5936514b2a02e779bd22
Resaki1 <christian.reski@t-online.de>
2023-08-04 16:01:00 +0200

    added basic component

---
+++ src/components/Uniqueness.tsx
@@ -0,0 +1,46 @@
+export const Uniqueness = () => {
+  return (
+    <section className="uniqueness__wrapper">
+      <h2>Was macht Scrumlr so besonders?</h2>
+      <p>
+        Scrumlr fördert eine ausgewogene Zusammenarbeit innerhalb jedes Teams.{" "}
+      </p>
+      <div>
+        <div>
+          <h3>Schnellstart: ohne Registrierung und kostenlos</h3>
+          <p>
+            Starte direkt und unverbindlich, ohne aufwändige Registrierung oder
+            Anmeldung. Scrumlr ist außerdem kostenlos – für immer!
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h3>Individuelle Denkweisen mit dem Team teilen</h3>
+          <p>
+            Dieses Tool ist der beste Weg, Ideen miteinander auszutauschen und
+            sich als Team weiterzuentwickeln.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h3>Meinungen und Gedanken übersichtlich sammeln</h3>
+          <p>
+            Transparente Kommunikation ist wichtig. Mit Scrumlr könnt ihr
+            Meinungen, Gedanken und Ideen miteinander verknüpfen.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h3>Open Source & sicher gehostet in Deutschland</h3>
+          <p>
+            Scrumlr ist ein Open-Source-Software, deren Quelldaten eingesehen,
+            geändert und von Dritten verwendet werden können.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
