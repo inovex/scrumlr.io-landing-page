2fdb45e02e5bfcb44db93ece796549b86d152bae
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-08-13 13:17:46 +0200

    Changes

--- src/views/ReachNewHeights/ReachNewHeights.tsx
+++ src/views/ReachNewHeights/ReachNewHeights.tsx
@@ -5,7 +5,35 @@ export const ReachNewHeights = () => {
     <section className='reach-new-heights'>
       <h1>Erreiche mit deinem <span>Team</span> neue Erfolge und startet eure erste <span>Retrospektive.</span></h1>
       <p>Beginnt unmittelbar eure erste gemeinsame Sitzung - es ist keine Registrierung erforderlich und es ist absolut kostenlos.</p>
+      <img 
+        src='assets/images/reach-new-heights_light.svg' 
+        alt='Reach new heights' 
+        className='reach-new-heights__image reach-new-heights__image--light'
+      />
+      <img
+        src='assets/images/reach-new-heights_dark.svg' 
+        alt='Reach new heights'
+        className='reach-new-heights__image reach-new-heights__image--dark'
+      />
+      <div className='reach-new-heights__content'>
+        <div className='reach-new-heights__content__part1'>
+        </div>
+        <div className='reach-new-heights__information'>
+          <div className='reach-new-heights__information-item'>
+            <img src='assets/icons/icon_what-is-it-about_light.svg' alt='What is it about?' className='information-item__icon information-item__icon--light' />
+            <img src='assets/icons/icon_what-is-it-about_dark.svg' alt='What is it about?' className='information-item__icon information-item__icon--dark' />
+            <h2>Worum geht es?</h2>
+            <p>Scrumlr ist ein kooperatives Tool zum <b>Austausch von Ideen, Meinungen und Feedback.</b></p>
+          </div>
 
+          <div className='reach-new-heights__information-item'>
+            <img src='assets/icons/icon_how-can-it-be-used_light.svg' alt='How can it be used?' className='information-item__icon information-item__icon--light' />
+            <img src='assets/icons/icon_how-can-it-be-used_dark.svg' alt='How can it be used?' className='information-item__icon information-item__icon--dark' />
+            <h2>Wie wird es genutzt?</h2>
+            <p>Scrumlr ist jederzeit und auf allen Endgeräten verfügbar - einfach <b>Website aufrufen und auf Start klicken.</b></p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
