0452a6fd77ca38ed712deb6e4e283731bae1d9a6
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-08-13 11:58:20 +0200

    Intial commit

---
+++ src/views/ReachNewHeights/ReachNewHeights.tsx
@@ -0,0 +1,11 @@
+import './ReachNewHeights.scss';
+
+export const ReachNewHeights = () => {
+  return (
+    <section className='reach-new-heights'>
+      <h1>Erreiche mit deinem <span>Team</span> neue Erfolge und startet eure erste <span>Retrospektive.</span></h1>
+      <p>Beginnt unmittelbar eure erste gemeinsame Sitzung - es ist keine Registrierung erforderlich und es ist absolut kostenlos.</p>
+
+    </section>
+  );
+}
