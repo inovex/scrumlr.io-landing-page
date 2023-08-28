4efbe511d713cb07ab7466472af2123fda52a45e
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-08-28 15:34:41 +0200

    Add arrow right icon

---
+++ src/icons/ArrowRight.tsx
@@ -0,0 +1,8 @@
+const ArrowRight = () => (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 4L13 10L7 16" stroke="currentColor" stroke-width="2"/>
+  </svg>
+
+);
+
+export default ArrowRight;
