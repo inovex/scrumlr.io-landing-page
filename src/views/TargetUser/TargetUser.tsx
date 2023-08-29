72df365a9423c48d4be78c9fe94a40eac58078d7
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-08-29 13:11:05 +0200

    Implement TargetUser section with styling

---
+++ src/views/TargetUser/TargetUser.tsx
@@ -0,0 +1,10 @@
+import './TargetUser.scss';
+
+export const TargetUser = () => (
+  <section className='target-user'>
+    <h2>Wir sind alle wichtig!</h2>
+    <p>Jede:r kann Scrumlr nutzen und dadurch die Teamarbeit stärken.</p>
+    <img className='target-user__image--light' src='/public/assets/images/target-user_light.gif' alt='Winkende Figuren' />
+    <img className='target-user__image--dark' src='/public/assets/images/target-user_dark.gif' alt='Winkende Figuren' />
+  </section>
+);
