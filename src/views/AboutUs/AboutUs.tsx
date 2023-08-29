5a2b7984d7735818010d307a571016a4b897d729
Resaki1 <christian.reski@t-online.de>
2023-08-29 17:18:49 +0200

    added basic about us component

---
+++ src/views/AboutUs/AboutUs.tsx
@@ -0,0 +1,40 @@
+import { Button } from "../../components/Button/Button";
+import "./AboutUs.scss";
+
+export const AboutUs = () => {
+  return (
+    <section className="about-us">
+      <div className="about-us__scrumlr">
+        <div className="about-us__scrumlr-text">
+          <h2>Wir sind Scrumlr</h2>
+          <p>
+            Mit dem Scruml-Tool geben wir jedem Team die Möglichkeit einer
+            zielgerichteten Retrospektive. Wir streben danach, Kommunikation,
+            agile Vorgehensweisen und fachspezifische Expertise
+            zusammenzubringen. Jedes einzelne Teammitglied und dessen Meinung
+            zählt – denn nur so kann allgemeine Zufriedenheit erreicht werden.
+            <br />
+            <span className="bold">Seid auch dabei</span> und ermöglicht eurem
+            Team einen wirkungsvollen Rückblick auf bereits Erlebtes!
+          </p>
+        </div>
+        <div className="about-us__scrumlr-image">
+          <img
+            src="/assets/about/stan.webp"
+            alt="Das Faultier Stan mit einer Kaffeetasse und einem Stift"
+          />
+        </div>
+      </div>
+      <div className="about-us__cta">
+        <h2>Selber loslegen und überzeugen lassen.</h2>
+        <p>
+          Startet noch heute und ermöglicht euch eine einwandfreie
+          Retrospektive.
+        </p>
+        <Button ariaLabel="Jetzt loslegen" className="about-us__cta-button">
+          Jetzt loslegen
+        </Button>
+      </div>
+    </section>
+  );
+};
