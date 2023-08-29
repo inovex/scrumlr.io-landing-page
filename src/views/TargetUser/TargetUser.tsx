b14fdb7e7b676b342a53d1726bc582768c27dcf6
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-08-29 15:25:28 +0200

    Changes

--- src/views/TargetUser/TargetUser.tsx
+++ src/views/TargetUser/TargetUser.tsx
@@ -4,7 +4,7 @@ export const TargetUser = () => (
   <section className='target-user'>
     <h2>Wir sind alle wichtig!</h2>
     <p>Jede:r kann Scrumlr nutzen und dadurch die Teamarbeit stärken.</p>
-    <img className='target-user__image--light' src='/public/assets/images/target-user_light.gif' alt='Winkende Figuren' />
-    <img className='target-user__image--dark' src='/public/assets/images/target-user_dark.gif' alt='Winkende Figuren' />
+    <img className='target-user__image--light' src='/assets/images/target-user_light.gif' alt='Winkende Figuren' />
+    <img className='target-user__image--dark' src='/assets/images/target-user_dark.gif' alt='Winkende Figuren' />
   </section>
 );
