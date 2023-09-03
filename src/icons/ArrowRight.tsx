2aef57f55fe7e18a985cdbfe9d5c9877ac153950
Resaki1 <christian.reski@t-online.de>
2023-09-03 11:30:00 +0200

    fixed svg attributes

--- src/icons/ArrowRight.tsx
+++ src/icons/ArrowRight.tsx
@@ -1,8 +1,13 @@
 const ArrowRight = () => (
-  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M7 4L13 10L7 16" stroke="currentColor" stroke-width="2"/>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M7 4L13 10L7 16" stroke="currentColor" strokeWidth="2" />
   </svg>
-
 );
 
 export default ArrowRight;
