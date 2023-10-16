274e692af8e270133dcdc63997ec3a0e15d1db84
Christian Reski <35272402+Resaki1@users.noreply.github.com>
2023-10-16 12:40:46 +0200

    Features (#53)

---
+++ src/views/Features/images/Features_dark_1.tsx
@@ -0,0 +1,442 @@
+import "./Features_dark_1.scss";
+
+const Features_darK_1 = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width="355.89"
+    height="313.503"
+    viewBox="0 0 355.89 313.503"
+    id="features_dark_1"
+  >
+    <defs>
+      <filter
+        id="Rechteck_12531"
+        x="63.838"
+        y="21.452"
+        width="292.052"
+        height="292.052"
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy="3" input="SourceAlpha" />
+        <feGaussianBlur stdDeviation="3" result="blur" />
+        <feFlood floodOpacity="0.161" />
+        <feComposite operator="in" in2="blur" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="BG"
+        x="63.81"
+        y="21.293"
+        width="180.561"
+        height="87.943"
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy="3" input="SourceAlpha" />
+        <feGaussianBlur stdDeviation="3" result="blur-2" />
+        <feFlood floodColor="#111" floodOpacity="0.09" />
+        <feComposite operator="in" in2="blur-2" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="BG-card-staple"
+        x="10.441"
+        y="236.641"
+        width="209.518"
+        height="31.264"
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy="2" input="SourceAlpha" />
+        <feGaussianBlur stdDeviation="1.5" result="blur-3" />
+        <feFlood floodColor="#111" floodOpacity="0.161" />
+        <feComposite operator="in" in2="blur-3" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="BG-2"
+        x="0"
+        y="110.605"
+        width="232.147"
+        height="155.949"
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy="3" input="SourceAlpha" />
+        <feGaussianBlur stdDeviation="3" result="blur-4" />
+        <feFlood floodColor="#111" floodOpacity="0.22" />
+        <feComposite operator="in" in2="blur-4" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <filter
+        id="BG-3"
+        x="254.776"
+        y="0"
+        width="91.691"
+        height="236.428"
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy="3" input="SourceAlpha" />
+        <feGaussianBlur stdDeviation="5" result="blur-5" />
+        <feFlood floodColor="#111" floodOpacity="0.149" />
+        <feComposite operator="in" in2="blur-5" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+    </defs>
+    <g id="Grafik_kl" transform="translate(-502.022 -4810.081)">
+      <g
+        id="Gruppe_7998"
+        data-name="Gruppe 7998"
+        transform="translate(511.022 4822.081)"
+      >
+        <g
+          transform="matrix(1, 0, 0, 1, -9, -12)"
+          filter="url(#Rechteck_12531)"
+        >
+          <rect
+            id="Rechteck_12531-2"
+            data-name="Rechteck 12531"
+            width="274.052"
+            height="274.052"
+            rx="30"
+            transform="translate(72.84 27.45)"
+            fill="#1e2638"
+          />
+        </g>
+        <g id="Gruppe_7995" data-name="Gruppe 7995" transform="translate(0 0)">
+          <g
+            id="Gruppe_7990"
+            data-name="Gruppe 7990"
+            transform="translate(63.81 15.294)"
+          >
+            <g
+              id="Gruppe_7987"
+              data-name="Gruppe 7987"
+              transform="translate(0 0)"
+            >
+              <g
+                id="Gruppe_7988"
+                data-name="Gruppe 7988"
+                transform="translate(0)"
+              >
+                <g id="ca_card-staple">
+                  <g id="card">
+                    <g
+                      transform="matrix(1, 0, 0, 1, -72.81, -27.29)"
+                      filter="url(#BG)"
+                    >
+                      <path
+                        id="BG-4"
+                        data-name="BG"
+                        d="M30,0H154.561a8,8,0,0,1,8,8V61.943a8,8,0,0,1-8,8H0a0,0,0,0,1,0,0V30A30,30,0,0,1,30,0Z"
+                        transform="translate(72.81 27.29)"
+                        fill="#313949"
+                      />
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </g>
+          <g
+            id="Gruppe_7986"
+            data-name="Gruppe 7986"
+            transform="translate(0 104.605)"
+          >
+            <g
+              id="ca_card-staple-2"
+              data-name="ca_card-staple"
+              transform="translate(0 0)"
+            >
+              <g
+                transform="matrix(1, 0, 0, 1, -9, -116.61)"
+                filter="url(#BG-card-staple)"
+              >
+                <path
+                  id="BG-card-staple-2"
+                  data-name="BG-card-staple"
+                  d="M0,0H200.518a0,0,0,0,1,0,0V14.264a8,8,0,0,1-8,8H8a8,8,0,0,1-8-8V0A0,0,0,0,1,0,0Z"
+                  transform="translate(14.94 239.14)"
+                  fill="#313949"
+                />
+              </g>
+              <g id="card-2" data-name="card">
+                <g
+                  transform="matrix(1, 0, 0, 1, -9, -116.61)"
+                  filter="url(#BG-2)"
+                >
+                  <rect
+                    id="card-2-bg"
+                    data-name="BG"
+                    width="214.147"
+                    height="137.949"
+                    rx="8"
+                    transform="translate(9 116.61)"
+                    fill="#313949"
+                    stroke="transparent"
+                    strokeWidth="2"
+                  />
+                </g>
+              </g>
+            </g>
+          </g>
+          <g transform="matrix(1, 0, 0, 1, -9, -12)" filter="url(#BG-3)">
+            <path
+              id="BG-6"
+              data-name="BG"
+              d="M30.846,0A30.846,30.846,0,0,1,61.691,30.846V175.583a30.846,30.846,0,1,1-61.691,0V30.846A30.846,30.846,0,0,1,30.846,0Z"
+              transform="translate(269.78 12)"
+              fill="#313949"
+            />
+          </g>
+          <rect
+            id="Rechteck_12544"
+            data-name="Rechteck 12544"
+            width="114.785"
+            height="15.452"
+            rx="2"
+            transform="translate(15.428 129.044)"
+            fill="#202939"
+          />
+          <g
+            id="Gruppe_7986-2"
+            data-name="Gruppe 7986"
+            transform="translate(265.521 84.232)"
+          >
+            <g id="ic_vote" transform="translate(0 0)" opacity="0.2">
+              <rect
+                id="BG-7"
+                data-name="BG"
+                width="37.964"
+                height="37.964"
+                transform="translate(0 1.186)"
+                fill="rgba(255,255,255,0)"
+              />
+              <g id="ic" transform="translate(16.942)">
+                <path
+                  id="Pfad_38"
+                  data-name="Pfad 38"
+                  d="M6.594,12.636V1.4A1.4,1.4,0,0,1,9.4,1.4V6.524M6.665,18.179.152,11.373A1.4,1.4,0,0,1,.784,9.479,1.4,1.4,0,0,1,1.863,9.4c.356.119.457.054.622.391l4.109,2.847M9.4,7.017a1.4,1.4,0,0,1,2.807,0m.035,0c0-.775.593-.7,1.368-.7a1.4,1.4,0,0,1,1.4,1.4"
+                  transform="translate(0 4.745)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                />
+                <path
+                  id="Pfad_39"
+                  data-name="Pfad 39"
+                  d="M0,10.742a3.543,3.543,0,0,0,3.017,1.684H7.648a3.508,3.508,0,0,0,3.508-3.508V1.684C11.156.909,10.018,0,9.243,0"
+                  transform="translate(6.665 12.183)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                />
+                <path
+                  id="Pfad_40"
+                  data-name="Pfad 40"
+                  d="M9.491,4.745A4.745,4.745,0,0,0,0,4.745"
+                  transform="translate(3.22)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                />
+                <path
+                  id="Pfad_41"
+                  data-name="Pfad 41"
+                  d="M6.719,9.062a4.745,4.745,0,1,0-3.91.017"
+                  transform="translate(3.22)"
+                  fill="none"
+                  stroke="rgba(255,255,255,0.2)"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                />
+              </g>
+            </g>
+          </g>
+          <g
+            id="Gruppe_7997"
+            data-name="Gruppe 7997"
+            transform="translate(138.856 133.563)"
+          >
+            <g
+              id="Gruppe_4200"
+              data-name="Gruppe 4200"
+              transform="translate(0 0)"
+            >
+              <rect
+                id="Rechteck_1148"
+                data-name="Rechteck 1148"
+                width="189.803"
+                height="49.828"
+                rx="24.914"
+                transform="translate(-10)"
+                fill="#485064"
+              />
+              <text
+                id="Presenter_mode"
+                data-name="Presenter mode"
+                transform="translate(9.52 30.444)"
+                fill="#fff"
+                fontSize="14"
+                fontFamily="Raleway-Bold, Raleway"
+                fontWeight="700"
+                letterSpacing="0.035em"
+              >
+                <tspan x="0" y="0">
+                  Presenter mode
+                </tspan>
+              </text>
+            </g>
+            <g
+              id="Pfad_7378"
+              data-name="Pfad 7378"
+              transform="translate(129.976 0)"
+              fill="#313949"
+            >
+              <path
+                d="M 24.91378211975098 48.82756042480469 C 21.68500137329102 48.82756042480469 18.55333137512207 48.1954231262207 15.60576248168945 46.94871139526367 C 12.75825214385986 45.74431228637695 10.20071220397949 44.01990127563477 8.004181861877441 41.82338333129883 C 5.807662010192871 39.62685394287109 4.083251953125 37.06931304931641 2.878852128982544 34.2218017578125 C 1.632142066955566 31.27423286437988 1.000002145767212 28.14256286621094 1.000002145767212 24.91378211975098 C 1.000002145767212 21.68500137329102 1.632142066955566 18.55333137512207 2.878852128982544 15.60576248168945 C 4.083251953125 12.75825214385986 5.807662010192871 10.20071220397949 8.004181861877441 8.004181861877441 C 10.20071220397949 5.807662010192871 12.75825214385986 4.083251953125 15.60576248168945 2.878852128982544 C 18.55333137512207 1.632142066955566 21.68500137329102 1.000002145767212 24.91378211975098 1.000002145767212 C 28.14256286621094 1.000002145767212 31.27423286437988 1.632142066955566 34.2218017578125 2.878852128982544 C 37.06931304931641 4.083251953125 39.62685394287109 5.807662010192871 41.82338333129883 8.004181861877441 C 44.01990127563477 10.20071220397949 45.74431228637695 12.75825214385986 46.94871139526367 15.60576248168945 C 48.1954231262207 18.55333137512207 48.82756042480469 21.68500137329102 48.82756042480469 24.91378211975098 C 48.82756042480469 28.14256286621094 48.1954231262207 31.27423286437988 46.94871139526367 34.2218017578125 C 45.74431228637695 37.06931304931641 44.01990127563477 39.62685394287109 41.82338333129883 41.82338333129883 C 39.62685394287109 44.01990127563477 37.06931304931641 45.74431228637695 34.2218017578125 46.94871139526367 C 31.27423286437988 48.1954231262207 28.14256286621094 48.82756042480469 24.91378211975098 48.82756042480469 Z"
+                stroke="none"
+              />
+              <path
+                d="M 24.91378211975098 2.000003814697266 C 21.8195629119873 2.000003814697266 18.8189525604248 2.605560302734375 15.99532318115234 3.799861907958984 C 13.26700210571289 4.953842163085938 10.81630325317383 6.606281280517578 8.711292266845703 8.711292266845703 C 6.606281280517578 10.81630325317383 4.953842163085938 13.26700210571289 3.799861907958984 15.99532318115234 C 2.605560302734375 18.8189525604248 2.000003814697266 21.8195629119873 2.000003814697266 24.91378211975098 C 2.000003814697266 28.00800132751465 2.605560302734375 31.00861167907715 3.799861907958984 33.83224105834961 C 4.953842163085938 36.56056213378906 6.606281280517578 39.01126098632812 8.711292266845703 41.11627197265625 C 10.81630325317383 43.22128295898438 13.26700210571289 44.87372207641602 15.99532318115234 46.02770233154297 C 18.8189525604248 47.22200012207031 21.8195629119873 47.82756042480469 24.91378211975098 47.82756042480469 C 28.00800132751465 47.82756042480469 31.00861167907715 47.22200012207031 33.83224105834961 46.02770233154297 C 36.56056213378906 44.87372207641602 39.01126098632812 43.22128295898438 41.11627197265625 41.11627197265625 C 43.22128295898438 39.01126098632812 44.87372207641602 36.56056213378906 46.02770233154297 33.83224105834961 C 47.22200012207031 31.00861167907715 47.82756042480469 28.00800132751465 47.82756042480469 24.91378211975098 C 47.82756042480469 21.8195629119873 47.22200012207031 18.8189525604248 46.02770233154297 15.99532318115234 C 44.87372207641602 13.26700210571289 43.22128295898438 10.81630325317383 41.11627197265625 8.711292266845703 C 39.01126098632812 6.606281280517578 36.56056213378906 4.953842163085938 33.83224105834961 3.799861907958984 C 31.00861167907715 2.605560302734375 28.00800132751465 2.000003814697266 24.91378211975098 2.000003814697266 M 24.91378211975098 3.814697265625e-06 C 38.67328262329102 3.814697265625e-06 49.82756042480469 11.15428161621094 49.82756042480469 24.91378211975098 C 49.82756042480469 38.67328262329102 38.67328262329102 49.82756042480469 24.91378211975098 49.82756042480469 C 11.15428161621094 49.82756042480469 3.814697265625e-06 38.67328262329102 3.814697265625e-06 24.91378211975098 C 3.814697265625e-06 11.15428161621094 11.15428161621094 3.814697265625e-06 24.91378211975098 3.814697265625e-06 Z"
+                stroke="none"
+                fill="#485064"
+              />
+            </g>
+            <g id="ic_focus" transform="translate(135.908 5.932)">
+              <rect
+                id="BG-8"
+                data-name="BG"
+                width="37.964"
+                height="37.964"
+                fill="rgba(255,255,255,0)"
+                opacity="0.3"
+              />
+              <g id="ic-2" data-name="ic" transform="translate(6.199 7.118)">
+                <path
+                  id="Pfad_42"
+                  data-name="Pfad 42"
+                  d="M5.932,0,0,5.919"
+                  transform="translate(11.997 8.871)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                />
+                <path
+                  id="Pfad_43"
+                  data-name="Pfad 43"
+                  d="M0,0H3.546l.013.013V3.559"
+                  transform="translate(14.37 8.858)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeLinecap="square"
+                  strokeWidth="1.5"
+                />
+                <path
+                  id="Pfad_44"
+                  data-name="Pfad 44"
+                  d="M0,2.342A2.161,2.161,0,0,1,1.9,0H20.645a2.161,2.161,0,0,1,1.9,2.342v14.3a2.161,2.161,0,0,1-1.9,2.342H1.9A2.161,2.161,0,0,1,0,16.64Z"
+                  transform="translate(0 4.217)"
+                  fill="none"
+                  stroke="#fff"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                />
+                <path
+                  id="Pfad_45"
+                  data-name="Pfad 45"
+                  d="M0,.656,16.429.386"
+                  transform="translate(3.099 18.879)"
+                  fill="none"
+                  stroke="#989ba3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                />
+                <path
+                  id="Pfad_46"
+                  data-name="Pfad 46"
+                  d="M.571,0h1.1C4.342,0,7.69,3.264,7.69,5.827v1.8"
+                  transform="translate(19.167)"
+                  fill="none"
+                  stroke="#989ba3"
+                  strokeWidth="1.5"
+                />
+              </g>
+            </g>
+          </g>
+        </g>
+        <g id="ic_timer" transform="translate(272.639 22.044)" opacity="0.2">
+          <rect
+            id="BG-9"
+            data-name="BG"
+            width="37.964"
+            height="37.964"
+            fill="rgba(255,255,255,0)"
+            opacity="0.3"
+          />
+          <g id="ic-3" data-name="ic" transform="translate(9.491 7.118)">
+            <path
+              id="Pfad_32"
+              data-name="Pfad 32"
+              d="M12.649.538a9.5,9.5,0,1,0,3.775,2.471l2.037-1.823"
+              transform="translate(0 4.152)"
+              fill="none"
+              stroke="#fff"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            />
+            <path
+              id="Pfad_33"
+              data-name="Pfad 33"
+              d="M12.649.538a9.5,9.5,0,1,0,3.775,2.471"
+              transform="translate(0 4.152)"
+              fill="none"
+              stroke="#fff"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            />
+            <path
+              id="Pfad_34"
+              data-name="Pfad 34"
+              d="M0,1.232V1.186A1.186,1.186,0,0,1,1.186,0H4.745A1.186,1.186,0,0,1,5.932,1.186v.1"
+              transform="translate(6.525 0)"
+              fill="none"
+              stroke="#fff"
+              strokeLinecap="square"
+              strokeWidth="1.5"
+            />
+            <path
+              id="Pfad_35"
+              data-name="Pfad 35"
+              d="M.5,4.745V0"
+              transform="translate(8.991 9.491)"
+              fill="none"
+              stroke="#fff"
+              strokeLinecap="round"
+              strokeWidth="1.5"
+            />
+          </g>
+        </g>
+      </g>
+      <rect
+        id="Rechteck_12543"
+        data-name="Rechteck 12543"
+        width="83.881"
+        height="15.452"
+        rx="2"
+        transform="translate(526.45 4970.991)"
+        fill="#202939"
+      />
+    </g>
+  </svg>
+);
+
+export default Features_darK_1;
