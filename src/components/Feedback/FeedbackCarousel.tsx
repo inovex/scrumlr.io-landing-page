1bc111ba4d409570df619d5664777a489a2d3a0b
Resaki1 <christian.reski@t-online.de>
2023-10-19 15:18:54 +0200

    mobile layout fixes

--- src/components/Feedback/FeedbackCarousel.tsx
+++ src/components/Feedback/FeedbackCarousel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useSpringCarousel } from "react-spring-carousel";
 import type { FeedbackItem } from "./Feedback.astro";
 import useElementSize from "../../hooks/useElementSize";
