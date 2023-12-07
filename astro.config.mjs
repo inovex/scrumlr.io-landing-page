4627e38268a55bf76afd4c6c8903f9eadeb6f2e8
Resaki1 <christian.reski@t-online.de>
2023-12-07 12:29:26 +0100

    setting language route based on http request header now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
-
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
-    redirects: {
-        "/legal": "/legal/privacy",
-    },
+  integrations: [react()],
+  redirects: {
+    "/legal": "/legal/privacy",
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
