453dbe2184cb77b3d88ce66a09bd3ca1970cff34
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2023-12-08 08:42:02 +0100

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -8,8 +7,4 @@ export default defineConfig({
   redirects: {
     "/legal": "/legal/privacy",
   },
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
 });
