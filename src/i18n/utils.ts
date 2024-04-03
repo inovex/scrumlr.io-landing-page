2bac1c9a71ecdb3dd46aad0abb81c4ae143d8af7
Manuel Brandstetter <manuel.brandstetter@inovex.de>
2024-04-03 19:28:19 +0200

    Changes

---
+++ src/i18n/utils.ts
@@ -0,0 +1,17 @@
+const languages = {
+  en: 'English',
+  de: 'German',
+};
+
+const defaultLang = 'en';
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in languages) return lang as keyof typeof languages;
+  return defaultLang;
+}
+
+export function getPathForLanguage(lang: keyof typeof languages, path: string) {
+  return lang === defaultLang ? path : `/${lang}${path}`;
+}
+
