1ef6319b8558e4345d8237d779eecccc29368f12
Resaki1 <christian.reski@t-online.de>
2025-02-20 14:01:04 +0100

    using avatars hosted on our own CMS now

--- src/utils/directus.ts
+++ src/utils/directus.ts
@@ -1,33 +1,54 @@
-import {createDirectus, readItems, rest, staticToken} from '@directus/sdk';
+import {
+  createDirectus,
+  readFile,
+  readItems,
+  rest,
+  staticToken,
+} from "@directus/sdk";
 
 const directusUrl = import.meta.env.DIRECTUS_URL;
 const directusToken = import.meta.env.DIRECTUS_TOKEN;
 
 const directusClient = createDirectus(directusUrl)
-    .with(staticToken(directusToken))
-    .with(rest());
+  .with(staticToken(directusToken))
+  .with(rest());
 
-export default async function getTranslatedContent(collection: string, language: string) {
-    const content = await directusClient.request<{id: string, translations: [Record<string, any>]}>(readItems(collection, {
-        deep: {
-            translations: {
-                _filter: {
-                    languages_code: {
-                        _eq: language
-                    },
-                },
+export default async function getTranslatedContent(
+  collection: string,
+  language: string,
+) {
+  const content = await directusClient.request<{
+    id: string;
+    translations: [Record<string, any>];
+  }>(
+    readItems(collection, {
+      deep: {
+        translations: {
+          _filter: {
+            languages_code: {
+              _eq: language,
             },
+          },
         },
-        fields: ['*', 'translations.*'],
-        limit: 1
-    }));
+      },
+      fields: ["*", "translations.*"],
+      limit: 1,
+    }),
+  );
 
-    return content.translations[0];
+  return content.translations[0];
 }
 
 export async function getContent(collection: string) {
-    const content = await directusClient.request(readItems(collection, {
-      fields: ['*.*'],
-    }));
-    return content;
+  console.log(directusUrl, directusToken);
+
+  console.log(
+    `${directusUrl}/assets/${"cc030b8a-4742-41a6-930a-f2aa65959de2"}`,
+  );
+  const content = await directusClient.request(
+    readItems(collection, {
+      fields: ["*.*"],
+    }),
+  );
+  return content;
 }
